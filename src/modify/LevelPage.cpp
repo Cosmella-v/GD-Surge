b89cc126de9cc774cd44c5f1bdca91afd1752897
OmgRod <89850217+OmgRod@users.noreply.github.com>
2025-05-16 22:56:10 +0100

    levelpage fixes and changes

--- src/modify/LevelPage.cpp
+++ src/modify/LevelPage.cpp
@@ -20,49 +20,62 @@ void MyLevelPage::updateDynamicPage(GJGameLevel* level) {
     auto winSize = CCDirector::sharedDirector()->getWinSize();
 
     queueInMainThread([this, level, winSize]() {
-        if (auto scene = CCDirector::sharedDirector()->getRunningScene()) {
-            if (auto layer = scene->getChildByType<LevelSelectLayer*>(0)) {
-                if (level->m_levelID == -1) {
-                    if (auto label = typeinfo_cast<CCLabelBMFont*>(this->getChildByID("coming-soon-label"))) {
-                        if (AchievementManager::sharedState()->isAchievementEarned("geometry.ach.surge.vault04")) {
-                            label->setString("Coming Never...");
-                        } else {
-                            label->setString("Coming Soon!");
-                        }
-                        label->setAnchorPoint({0.5f, 0.5f});
-                        label->setPositionX(layer->getContentSize().width / 2);
-                    }
-                } else if (level->m_levelID == -3) {
-                    if (auto label = typeinfo_cast<CCLabelBMFont*>(this->getChildByID("coming-soon-label"))) {
-                        label->removeFromParent();
-                    }
+        auto scene = CCDirector::sharedDirector()->getRunningScene();
+        if (!scene) return;
 
+        auto layer = scene->getChildByType<LevelSelectLayer*>(0);
+        if (!layer) return;
+
+        auto label = typeinfo_cast<CCLabelBMFont*>(this->getChildByID("coming-soon-label"));
+
+        switch (level->m_levelID) {
+            case -1:
+                if (label) {
+                    bool hasAch = AchievementManager::sharedState()->isAchievementEarned("geometry.ach.surge.vault04");
+                    label->setString(hasAch ? "Coming Never..." : "Coming Soon!");
+                    label->setAnchorPoint({0.5f, 0.5f});
+                    label->setPositionX(layer->getContentSize().width / 2);
+                }
+                break;
+
+            case -3:
+                if (!this->getChildByID("islands-menu"_spr)) {
                     auto logo = CCSprite::createWithSpriteFrameName("islandsLogo.png"_spr);
                     logo->setScale(0.75f);
 
-                    if (!this->getChildByID("islands-menu"_spr)) {
-                        auto logoBtn = CCMenuItemSpriteExtra::create(
-                            logo,
-                            this,
-                            menu_selector(MyLevelPage::onIslands)
-                        );
-                        logoBtn->setPosition({ winSize.width / 2.f, winSize.height / 3.f * 2.f });
+                    auto logoBtn = CCMenuItemSpriteExtra::create(
+                        logo,
+                        this,
+                        menu_selector(MyLevelPage::onIslands)
+                    );
+                    logoBtn->setPosition({ winSize.width / 2.f, winSize.height * 2.f / 3.f });
 
-                        auto menu = CCMenu::create();
-                        menu->addChild(logoBtn);
-                        menu->setPosition({0, 0});
-                        menu->setID("islands-menu"_spr);
-                        this->addChild(menu);
-                    }
+                    auto menu = CCMenu::create();
+                    menu->addChild(logoBtn);
+                    menu->setPosition({0, 0});
+                    menu->setID("islands-menu"_spr);
+                    this->addChild(menu);
                 }
+                break;
+
+            default:
+                break;
+        }
+
+        if (level->m_levelID != -3) {
+            if (auto menu = this->getChildByID("islands-menu"_spr)) {
+                menu->removeFromParent();
             }
         }
+
+        m_isBusy = level->m_levelID < 0;
+        label->setVisible(level->m_levelID == -1);
     });
 }
 
 void MyLevelPage::onIslands(CCObject*) {
     FLAlertLayer::create(
-        "Islands",
+        "The Islands",
         "This <cg>feature</c> is a <cb>work in progress</c> and is not yet available. Sorry for the inconvenience.",
         "OK"
     )->show();
