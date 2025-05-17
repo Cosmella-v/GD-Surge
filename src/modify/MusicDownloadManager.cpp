f85e49468e8874e4c9719a4952b506ce6f3e7766
OmgRod <89850217+OmgRod@users.noreply.github.com>
2025-05-17 16:34:40 +0100

    bugfix

--- src/modify/MusicDownloadManager.cpp
+++ src/modify/MusicDownloadManager.cpp
@@ -2,14 +2,14 @@
 
 using namespace geode::prelude;
 
-gd::string MusicDownloadManager::pathForSFXFolder(int sfxID) {
+gd::string MyMusicDownloadManager::pathForSFXFolder(int sfxID) {
     std::filesystem::path saveDir = Mod::get()->getSaveDir();
     std::string path = (saveDir / "sfx").string();
     log::debug("sfx path: {}", path);
     return gd::string(path);
 }
 
-gd::string MusicDownloadManager::pathForSongFolder(int songID) {
+gd::string MyMusicDownloadManager::pathForSongFolder(int songID) {
     std::filesystem::path saveDir = Mod::get()->getSaveDir();
     std::string path = (saveDir / "songs").string();
     log::debug("song path: {}", path);
