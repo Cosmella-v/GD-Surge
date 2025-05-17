6c7d8382a21ff02cee77d549f58d5f8391701eaf
OmgRod <89850217+OmgRod@users.noreply.github.com>
2025-05-17 15:35:47 +0100

    fixed another mistake

--- src/modify/GJGarageLayer.cpp
+++ src/modify/GJGarageLayer.cpp
@@ -3,8 +3,13 @@
 using namespace geode::prelude;
 
 bool MyGJGarageLayer::init() {
+    if (!GJGarageLayer::init())
+        return false;
+    
     auto menu = this->getChildByID("top-left-menu");
     menu->getChildByID("shop-button")->setVisible(false);
+
+    return true;
 }
 
 void MyGJGarageLayer::showUnlockPopup(int p0, UnlockType p1) {
