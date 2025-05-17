946f42e03bf66f46e924c19505fcc4eee54cfbf0
OmgRod <89850217+OmgRod@users.noreply.github.com>
2025-05-17 11:06:33 +0100

    chumiu pls reply to my discord message (sry if i'm not allowed to use this code)

--- src/modify/LoadingLayer.cpp
+++ src/modify/LoadingLayer.cpp
@@ -6,25 +6,45 @@ bool MyLoadingLayer::init(bool fromReload) {
     if (!LoadingLayer::init(fromReload))
         return false;
 
-    // auto director = CCDirector::get();
-    // auto winSize = director->getWinSize();
-    // auto GM = GameManager::sharedState();
+    auto GM = GameManager::sharedState();
+    auto SFC = CCSpriteFrameCache::get();
+    auto searchPathRoot = dirs::getModRuntimeDir() / Mod::get()->getID() / "resources";
 
-    // auto mainTitle = getChildByType<CCSprite>(1);
-    // if (!mainTitle)
-    //     return true;
+    CCFileUtils::sharedFileUtils()->addSearchPath(searchPathRoot.string().c_str());
+    SFC->addSpriteFramesWithFile("TitleSheet.plist"_spr);
 
-    // CCSprite* surgeLogo = CCSprite::createWithSpriteFrameName("surgeLogo_001.png"_spr);
-    // if (!surgeLogo) {
-    //     log::warn("Sprite frame 'surgeLogo_001.png' not found in cache.");
-    //     return true;
-    // }
+    auto version = CCLabelBMFont::create(fmt::format("{}", Mod::get()->getVersion().toVString(true)).c_str(), "goldFont.fnt");
+    version->setPosition({5, 5});
+    version->setAnchorPoint({0, 0});
+    version->setScale(0.5f);
+    this->addChild(version);
 
-    // CCRect logoBounds = mainTitle->boundingBox();
-    // surgeLogo->setAnchorPoint({1.0f, 1.0f});
-    // surgeLogo->setPosition(ccp(logoBounds.getMaxX() - 2.f, logoBounds.getMinY() - 1.5f));
+    auto director = CCDirector::get();
+    auto winSize = director->getWinSize();
 
-    // this->addChild(surgeLogo);
+    auto mainTitle = getChildByType<CCSprite>(1);
+    mainTitle->setPositionY(mainTitle->getPositionY() + 5.f);
+    if (!mainTitle)
+        return true;
+
+    CCSprite* surgeLogo = CCSprite::createWithSpriteFrameName("surgeLogo_001.png"_spr);
+    if (!surgeLogo) {
+        log::warn("Sprite frame 'surgeLogo_001.png' not found in cache.");
+        return true;
+    }
+
+    CCRect logoBounds = mainTitle->boundingBox();
+    surgeLogo->setAnchorPoint({1.0f, 1.0f});
+    surgeLogo->setPosition(ccp(logoBounds.getMaxX() - 2.f, logoBounds.getMinY() - 1.5f));
+
+    this->addChild(surgeLogo);
+
+    auto robtopLogo = static_cast<CCSprite *>(this->getChildByID("robtop-logo"));
+    if (robtopLogo)
+    {
+        auto teamLogo = CCSprite::createWithSpriteFrameName("omgrodLogo_01.png"_spr);
+        robtopLogo->setDisplayFrame(teamLogo->displayFrame());
+    };
 
     return true;
 }
