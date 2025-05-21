4d4c6d9b169867af3cfa5d9e97a3e4ed81fb954a
OmgRod <89850217+OmgRod@users.noreply.github.com>
2025-05-21 18:07:50 +0100

    prep for the shattering exclusive level

--- src/modify/LocalLevelManager.cpp
+++ src/modify/LocalLevelManager.cpp
@@ -4,7 +4,7 @@ using namespace geode::prelude;
 
 gd::string MyLocalLevelManager::getMainLevelString(int id) {
 
-    auto file = CCString::createWithFormat("level-%i.txt"_spr, id);
+    auto file = CCString::createWithFormat("level%i.txt"_spr, id);
     if(file == nullptr) return "";
     auto content = CCString::createWithContentsOfFile(file->getCString());
     if(content == nullptr) return "";
